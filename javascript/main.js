c16451a5de373dcd7fc72c8196adb824330d9e32
JerryWski <jarek_wasilewski@yahoo.com>
2022-10-23 23:22:33 +0200

    js

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,18 @@
-const socialBarAvatar = document.querySelector('.avatar-icon');
-const socialBarIcons = document.querySelector('.social-icons');
-const shareBtn = document.querySelector('.social__btn');
-// const activeBar = document.querySelector('.active');
-console.log();
+const socialBarAvatar = document.querySelector(".social");
+const socialBarIcons = document.querySelector(".social-icons");
+const shareBtn = document.querySelector(".social__btn");
+const shareBtnTwo = document.querySelector(".active-btn");
+console.log(shareBtnTwo);
 
+const toggleHandler = () => {
+  socialBarAvatar.style.display = "none";
+  socialBarIcons.style.display = "flex";
+};
 
+const toggleHanderTwo = () => {
+  socialBarIcons.style.display = "none";
+  socialBarAvatar.style.display = "flex";
+};
 
+shareBtn.addEventListener("click", toggleHandler);
+shareBtnTwo.addEventListener("click", toggleHanderTwo);
