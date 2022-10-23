0117a8fc764c6adc3b1e92cfaf3eb8e89c2e07fb
JerryWski <jarek_wasilewski@yahoo.com>
2022-10-23 22:59:40 +0200

    fixing some stuff

--- javascript/main.js
+++ javascript/main.js
@@ -1,14 +1,8 @@
 const socialBarAvatar = document.querySelector('.avatar-icon');
 const socialBarIcons = document.querySelector('.social-icons');
 const shareBtn = document.querySelector('.social__btn');
-const activeBar = document.querySelector('.active')
+// const activeBar = document.querySelector('.active');
+console.log();
 
 
-
-
-shareBtn.addEventListener('click', () => {
-    if(activeBar.classList.contains('active')) {
-        socialBarIcons.style.display='flex'
-    }
-})
 
